df91e759b67283b017a79c1476a798766b63a8ad
Ercius Dell Precision <percius@lbl.gov>
2023-06-01 10:51:39 -0700

    improve package layout

---
+++ ncem_browser/run_ncem_browser.py
@@ -0,0 +1,7 @@
+"""A script to run the GUI. Mainly for debugging. The entrypoint script is the preferred way of
+running program. Usually typing ncem_browser at the command line works."""
+
+from ncem_browser import main
+
+if __name__ == '__main__':
+    main()
