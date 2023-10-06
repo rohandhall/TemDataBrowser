6fd5403c2c16b8957d7d18795ac5b34043dcadc5
Peter Ercius <percius@lbl.gov>
2023-10-06 09:47:47 -0700

    Update imports

--- run_TemDataBrowser.py
+++ run_TemDataBrowser.py
@@ -1,7 +1,6 @@
-"""A script to run the GUI. Mainly for debugging. The entrypoint script is the preferred way of
-running program. Usually typing ncem_browser at the command line works."""
+"""A script to run the GUI. Mainly for debugging."""
 
-from ncem_browser import main
+from TemDataBrowser import main
 
 if __name__ == '__main__':
     main()
